47975753923257336011300081f89afa30746299
Richard Giddings <mail@richardgiddings.co.uk>
2018-04-18 16:12:48 +0100

    Main page. The "Show Another" button calls the index view again to get a random NiceThing.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import NiceThing
 
-# Create your views here.
+def index(request):
+    # get a random quote
+    nice_thing = NiceThing.objects.order_by('?').first()
+
+    return render(request, 
+                template_name="main/index.html",
+                context={'nice_thing': nice_thing})
