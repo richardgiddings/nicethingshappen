69fe2852d6fe99261d0aad4e3051d5e017d2bb74
Richard Giddings <mail@richardgiddings.co.uk>
2018-04-18 12:51:36 +0100

    Model and migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class NiceThing(models.Model):
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    reported = models.BooleanField(default=False)
+    reported_at = models.DateTimeField()
+
+    # Reporting inappropriate posts 
+    #
+    # If a NiceThing has a reported value of True then we need to 
+    # look at the content. This will be a seperate app to review
+    # reported posts with a DELETE and OK buttons. The reported_at date
+    # can be used to prioritise reviewing.
+
+    def __str__(self):
+        return "{} {}".format(date_added.strftime("%d-%m-%Y"), self.text[20])
