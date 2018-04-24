94f2aa5cd448af3ccc3344dfa4459023ff833caa
Richard Giddings <mail@richardgiddings.co.uk>
2018-04-24 14:06:40 +0100

    Allowed hosts set to www.nicethingshappen.co.uk in production

--- nicethingshappen/settings/production.py
+++ nicethingshappen/settings/production.py
@@ -11,7 +11,7 @@ env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
 
 # Allow all host headers for now until we have a domain name
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.nicethingshappen.co.uk']
 
 DEBUG = False
 
